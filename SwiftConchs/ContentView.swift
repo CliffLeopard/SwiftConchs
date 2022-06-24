d36868b7cd68e8d0d124ddd41de60200d88aad21
CliffLeoaprd <cliffleopard@yahoo.com>
2022-06-24 11:00:01 +0800

    Initial Commit

---
+++ SwiftConchs/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftConchs
+//
+//  Created by CliffLeopard on 2022/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftConchs/SwiftConchsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConchsApp.swift
+//  SwiftConchs
+//
+//  Created by CliffLeopard on 2022/6/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConchsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
